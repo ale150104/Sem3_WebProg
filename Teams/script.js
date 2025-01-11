9e2d99f6fecba2f1233f93666d9f769e85097e1d
Luca Rößler <lr235760@hs-weingarten.de>
2025-01-11 16:52:08 +0100

    Team Seite

---
+++ Teams/script.js
@@ -0,0 +1,9 @@
+
+function switchToMaenner(){
+    window.location.href = '/Teams/maenner.html'; // Weiterleitung zur neuen Seite
+};
+
+function switchToFrauen(){
+    window.location.href = '/Teams/frauen.html';
+
+}
