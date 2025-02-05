bfc4152ae574dc877314e5a58519f16a1a95f355
Alessio Palumbo <apalumbo1501@gmail.com>
2025-02-05 15:51:41 +0100

    .

--- aktuelleSpiele/aktuelleSpiele.js
+++ aktuelleSpiele/aktuelleSpiele.js
@@ -1,3 +1,483 @@
+
+// Objekt mit den Spielen
+
+let games = [
+   {
+      additionalInfo: "https://www.goal.com/de/spiel/elversberg-vs-schalke-04/aufstellungen/F6cmWkz6lpVSOmNosXcQA",
+      DateOfGame: "Fr., 20.12.2024, 18:30 Uhr",
+      LocationOfGame: "URSAPHARM-Arena",
+      Logo1URL: "../assets/schalke.png",
+      Name1: "Schalke 04",
+      Goals1: 4,
+      Players1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "J.Heekeren"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "R. Schallenberg"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "D. Murkin"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "T. Bulut"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Kaminski"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "J. Bachmann"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "C. Antwi-Adjej"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "M. Aydin"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "K. Karaman"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "K. Max Grüger"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Sylla"
+         },
+      ],
+      Reserve1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : " l.Hamache"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " R. Hoffmann"
+         },
+   
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " T. Kalas"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " ..."
+         },
+      ],
+      Logo2URL: "../assets/Elversberg.png",
+      Name2: "Elversberg",
+      Goals2: 1,
+      Players2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "N. Kristof"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "M. Neubauer"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "E. Baum"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "L. Pinckert"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "M. Rohr"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "T. Zimmerschied"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Damar"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "L. Petkov"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "F. Schmahl"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "C. Sickinger"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "F. Asllani"
+         },
+      ],
+      Reserve2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "L. Schnellbacher"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "Luca Dürtholtz"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "F. Gerezgiher"
+     },
+     {
+      URLImage: "../assets/Person_Image_Placeholder.png",
+      Name : "..."
+   },
+      ],
+   
+   },
+   {
+      additionalInfo: "https://www.goal.com/de/spiel/braunschweig-vs-schalke-04/aufstellungen/5f7-nlEajoG36Oz0Jn3XJ",
+      DateOfGame: "Do., 18.01.2025, 13:00 Uhr",
+      LocationOfGame: "Eintracht Stadion",
+      Logo1URL: "../assets/schalke.png",
+      Name1: "Schalke 04",
+      Goals1: 0,
+      Players1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "J.Heekeren"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "R. Schallenberg"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "D. Murkin"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "T. Bulut"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Kaminski"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "P. Seguin"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "C. Antwi-Adjej"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "M. Aydin"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "K. Karaman"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "K. Max Grüger"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Sylla"
+         },
+      ],
+      Reserve1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : " l.Hamache"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " T. Mohr"
+         },
+   
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " T. Kalas"
+         },
+      ],
+      Logo2URL: "../assets/Logo_Eintracht_Braunschweig.png",
+      Name2: "Braunschweig",
+      Goals2: 0,
+      Players2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "M. Johansson"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "Marvin Rittmüller"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "R. Ivanov"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "J. Nikolaou"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "E. Bicakcic"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "F. Di Michele"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "L. Tempelmann"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "R.Philippe"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "Sven Köhler"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "K. Kaufmann"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "L. Szabo"
+         },
+      ],
+      Reserve2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "P. Jaeckel"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "J. Gomez"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "S. Polter"
+     },
+      ],
+   
+   },
+
+   {
+      additionalInfo: "https://www.goal.com/de/spiel/schalke-04-vs-1-fc-nuernberg/6_i3s5e7jLFYFzSJNfKmM",
+      DateOfGame: "Do., 25.01.2025, 13:00 Uhr",
+      LocationOfGame: "VELTINS-Arena",
+      Logo1URL: "../assets/schalke.png",
+      Name1: "Schalke 04",
+      Goals1: 3,
+      Players1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "J.Heekeren"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "R. Schallenberg"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "A. Donkor"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "T. Bulut"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Kaminski"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "P. Seguin"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "T. Mohr"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "M. Aydin"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "K. Karaman"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "K. Max Grüger"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "M. Sylla"
+         },
+      ],
+      Reserve1: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : " l.Hamache"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " F. Sanchez"
+         },
+   
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : " L. Karius"
+         }
+      ],
+      Logo2URL: "../assets/1._FC_Nürnberg_logo.png",
+      Name2: "Nürnberg",
+      Goals2: 1,
+      Players2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "J. Reichert"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "F. Jeltsch"
+          },
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "R. Knoche"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "O. Karafiat"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "J. Justvan"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "F. Flick"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "J. Castrop"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "D. Soares"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "O. Villadsen"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "C. Jander"
+         },
+         {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "S. Tzimas"
+         },
+      ],
+      Reserve2: [
+          {
+              URLImage: "../assets/Person_Image_Placeholder.png",
+              Name : "N. Seidel"
+          },
+          {
+            URLImage: "../assets/Person_Image_Placeholder.png",
+            Name : "R. Lubach"
+        },
+        {
+         URLImage: "../assets/Person_Image_Placeholder.png",
+         Name : "S. Joachims"
+     }
+      ],
+   
+   },
+   
+];
+
+
+
+
+var slideIndex = 0;
+function carousel() {
+   var i;
+   var y = document.getElementsByClassName("gameDetails");
+
+   var x = new Array();
+
+   console.log(y);
+   for(let i = 0; i < y.length; i++)
+   {
+         console.log(y[i].style.display);
+         if(y[i].style.display == "block")
+         {
+            let temp1 =  y[i].children[2].children;
+            for(let a = 0; a < temp1.length; a++){
+               x.push(temp1[a]);
+               console.log(temp1[a]);
+            }
+            
+         }
+   }
+
+   console.log("Hier kommt x");
+   console.log(x);
+   if(x.length > 0)
+   {
+         for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none"; 
+         }
+         slideIndex++;
+         if (slideIndex > x.length) {slideIndex = 1} 
+         x[slideIndex-1].style.display = "block"; 
+   }
+
+   // scaleImage("schalke", 1.5);
+   setTimeout(carousel, 5000); 
+   // scaleImage("schalke", 1.00);
+}
+
+function buildGamesView()
+{
+
+   let container = document.getElementById("container");
+   console.log("hallo")
+   if(container == null)
+   {
+         return;
+   }
+   console.log("Geschafft")
+
+   games.forEach((game) => {
+         console.log("ich mach eine build");
+         let singleGame = buildSingleGameView(game);
+         container.appendChild(singleGame);
+
+   })
+}
+
+buildGamesView();
+carousel();
+
+// Eine Spiel-Karte im gesamten Bauen, steuert die Funtionsaufrufe
  function buildSingleGameView(gameObj)
  {
     let gameContainer = document.createElement("article");
@@ -31,7 +511,7 @@
 
 
 
-
+ // Oberer Teil mit Kurz-Info zu einem Spiel
  function buildShortInfoView(gameObj, randomID)
  {
     
@@ -118,6 +598,7 @@
 
  }
 
+ // Details, also der untere Teil einer Karte, bauen
  function buildGameDetails(gameObj, randomID)
  {
     let gameDetails = document.createElement("article");
@@ -127,6 +608,24 @@
 
     let hr = document.createElement("hr");
     gameDetails.appendChild(hr);
+
+    let optionalLocationAndTimeOGame = document.createElement("section");
+    optionalLocationAndTimeOGame.classList.add("LocationAndTimeInfos");
+    optionalLocationAndTimeOGame.classList.add("w3-hide-medium");
+    optionalLocationAndTimeOGame.classList.add("w3-hide-large");
+
+    let location = document.createElement("article");
+    location.classList.add("gameLocation");
+    location.innerHTML = "📍 " + gameObj.LocationOfGame;
+
+    let dateAndTime = document.createElement("article");
+    dateAndTime.classList.add("dateAndTime");
+    dateAndTime.innerHTML = "⏰ " + gameObj.DateOfGame;
+
+    optionalLocationAndTimeOGame.appendChild(location);
+    optionalLocationAndTimeOGame.appendChild(dateAndTime);
+
+    gameDetails.appendChild(optionalLocationAndTimeOGame);
 
     let detailedInfos = document.createElement("section");
     detailedInfos.classList.add("detailedInfos");
@@ -162,20 +661,25 @@
  }
 
 
-
+ // Spieler-Details bauen
  function detailsOfATeam(teamName, startingPlayers, ReservePlayers)
   {
     let InfoTeam = document.createElement("article");
     InfoTeam.classList.add("mySlides");
     InfoTeam.classList.add("w3-animate-right");
 
-    let title = document.createElement("h5");
-    title.innerHTML = "Aufstellung " + teamName;
+   //  let title = document.createElement("h5");
+   //  title.innerHTML = "Aufstellung " + teamName;
 
-    InfoTeam.appendChild(title);
+   //  InfoTeam.appendChild(title);
 
     let startingEleven = document.createElement("section");
     startingEleven.classList.add("Player-Informations");
+
+      let title = document.createElement("h5");
+    title.innerHTML = "Aufstellung " + teamName;
+
+    startingEleven.appendChild(title);
 
     //------------------------
 
@@ -200,6 +704,8 @@
     return InfoTeam;
   }
 
+
+  // Einzelne Spieler-Infos bauen
  function appendPlayers(players, HtmlElement)
  {
     console.log("Die Spieler:");
@@ -260,3 +766,5 @@ function detailsVisibility(id)
    el.style.display = "block";
 
 }
+
+
